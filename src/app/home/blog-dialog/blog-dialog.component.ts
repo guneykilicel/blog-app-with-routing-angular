ba2fefa0b062192e7e3f0857ea8a591b8d30ff6e
guneykilicel <kilicelguney@gmail.com>
2023-07-14 17:35:57 +0300

    bitti

--- src/app/home/blog-dialog/blog-dialog.component.ts
+++ src/app/home/blog-dialog/blog-dialog.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog'
 import { CommentService } from 'src/app/services/comment.service';
 import { FormGroup, Validators, FormControl } from "@angular/forms";
+import { BlogService } from 'src/app/services/blog.service';
 
 @Component({
   selector: 'app-blog-dialog',
@@ -21,7 +22,7 @@ export class BlogDialogComponent {
     body: new FormControl(null, [Validators.required])
   })
 
-  constructor(private commentService: CommentService, @Inject(MAT_DIALOG_DATA) private data: any, private dialogRef: MatDialogRef<BlogDialogComponent>) {
+  constructor(private blogService: BlogService, private commentService: CommentService, @Inject(MAT_DIALOG_DATA) private data: any, private dialogRef: MatDialogRef<BlogDialogComponent>) {
     if(data.isUpdate) {
       this.isUpdate = true;
       this.form.patchValue({
@@ -42,7 +43,15 @@ export class BlogDialogComponent {
   }
 
   onSubmit() {
-    
+    const request = {
+      title: this.form.get('title')?.value,
+      body: this.form.get('body')?.value,
+      imageId: this.data.blog.imageId,
+      userId: this.data.blog.userId
+    }
+    this.blogService.updatePosts(this.data.blog.id, request).subscribe(res=>{
+      this.dialogRef.close();
+    })
   }
 
   close() {
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -26,6 +26,17 @@ export class HomeComponent {
   openDialog(element:any, vieworupdate: any) {
     const dialogRef = this.dialog.open(BlogDialogComponent, {
       data: {blog: element, isUpdate: vieworupdate}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.getBlogList();
+    })
+  }
+
+  getBlogList() {
+    this.blogService.getPosts().subscribe((res) => {
+      console.log(res);
+      this.blogData = res;
     })
   }
 }
